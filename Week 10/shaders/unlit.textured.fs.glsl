752c17540ad5844ac92392c3eac9cebe4cadb6e5
Rudolph Cacho Hernandez <112025281+rudycacho@users.noreply.github.com>
2024-11-04 14:29:16 -0800

    Step 5 Complete

---
+++ Week 10/shaders/unlit.textured.fs.glsl
@@ -0,0 +1,11 @@
+precision mediump float;
+
+uniform sampler2D uTexture;
+uniform float uAlpha;
+varying vec2 v_Textcoords;
+
+void main(void) {
+    gl_FragColor = texture2D(uTexture,v_Textcoords);
+}
+
+// EOF 00100001-10
---
+++ Week 10/shaders/unlit.textured.vs.glsl
@@ -0,0 +1,17 @@
+precision mediump float;
+
+attribute vec3 aVertexPosition;
+attribute vec2 aTexcoords;
+
+uniform mat4 uWorldMatrix;
+uniform mat4 uViewMatrix;
+uniform mat4 uProjectionMatrix;
+
+varying vec2 v_Textcoords;
+
+void main(void) {
+    gl_Position = uProjectionMatrix * uViewMatrix * uWorldMatrix * vec4(aVertexPosition, 1.0);
+    v_Textcoords = aTexcoords;
+}
+
+// EOF 00100001-10
