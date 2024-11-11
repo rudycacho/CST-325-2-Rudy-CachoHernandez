d1b8e92b07b899d3792ed7209acd2049c58accf9
Rudolph Cacho Hernandez <112025281+rudycacho@users.noreply.github.com>
2024-11-11 00:45:15 -0800

    Todo 9

--- Week 11/shaders/phong.directionlit.fs.glsl
+++ Week 11/shaders/phong.directionlit.fs.glsl
@@ -19,26 +19,32 @@ void main(void) {
     float lambert = dot(nLightDirection,nWorldNormal);
     // specular contribution
     // #4 in world space, calculate the direction from the surface point to the eye (normalized)
-    vec3 vEyePosition = normalize(uCameraPosition - vWorldPosition);
-    // todo #5 in world space, calculate the reflection vector (normalized)
-    // todo #6 calculate the phong term
+    vec3 vEyeDirection = normalize(uCameraPosition - vWorldPosition);
+    // #5 in world space, calculate the reflection vector (normalized)
+    vec3 vReflection = normalize(reflect(-vEyeDirection,nWorldNormal));
+    // #6 calculate the phong term
+    float phong = pow(max(dot(vReflection,vEyeDirection),0.0),64.0);
 
     // combine
-    // todo #7 apply light and material interaction for diffuse value by using the texture color as the material
-    // todo #8 apply light and material interaction for phong, assume phong material color is (0.3, 0.3, 0.3)
+    // #7 apply light and material interaction for diffuse value by using the texture color as the material
+
+    // #8 apply light and material interaction for phong, assume phong material color is (0.3, 0.3, 0.3)
 
 
     vec3 albedo = texture2D(uTexture, vTexcoords).rgb;
 
     vec3 ambient = albedo * 0.1;
-    // vec3 diffuseColor = todo
-    // vec3 specularColor = todo
+    vec3 diffuseColor = albedo * lambert;
+    vec3 specularColor = vec3(0.3,0.3,0.3) * phong;
 
-    // todo #9
+    // #9
     // add "diffuseColor" and "specularColor" when ready
-    vec3 finalColor = ambient; // + diffuseColor + specularColor;
+    vec3 finalColor = ambient + diffuseColor + specularColor;
 
-    gl_FragColor = vec4(vEyePosition, 1.0);
+    //gl_FragColor = vec4(vReflection, 1.0);
+    //gl_FragColor = vec4(phong,phong,phong, 1.0);
+    gl_FragColor = vec4(finalColor, 1.0);
+
 }
 
 // EOF 00100001-10
