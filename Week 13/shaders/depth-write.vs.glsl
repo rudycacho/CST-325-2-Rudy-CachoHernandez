faff3318654d65b62e9c74ec99b6b03eb8f193f4
Rudolph Cacho Hernandez <112025281+rudycacho@users.noreply.github.com>
2024-12-03 17:49:54 -0800

    todo #4

---
+++ Week 13/shaders/depth-write.vs.glsl
@@ -0,0 +1,17 @@
+
+    attribute vec3 aVertexPosition;
+
+    uniform mat4 uWorldMatrix;
+    uniform mat4 uViewMatrix;
+    uniform mat4 uProjectionMatrix;
+
+    varying float vDepth;
+
+    void main(void) {
+        gl_Position = uProjectionMatrix * uViewMatrix * uWorldMatrix * vec4(aVertexPosition, 1.0);
+
+        // todo #4 convert clip space depth into NDC and remap from [-1, 1] to [0, 1]
+        // The value of vDepth is temporarily set to gl_Position.z;
+        vDepth = ((gl_Position.z + 1.0) / 2.0);
+    }
+
